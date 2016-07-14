87d59de393fa06d15b2526f4e5fa96caa3e12f91
Lukas <lukas@DebianVM.LMeisegeier>
2016-07-14 13:15:33 +0200

    Added Header implementation and simple Post form

--- simple_project/simple_app/views.py
+++ simple_project/simple_app/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from django.template.context_processors import csrf
 from django.http import HttpResponse
+from django.http.request import QueryDict
+
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here
 
 def get_index(request):
-    return HttpResponse("Hello World "+request.GET['subject'])
+    #print(request.stream)
+    context={'name':'World'}
+    context.update(csrf(request))
+    print(request.body)
+    if 'name' in request.POST:
+        context['name']=request.POST['name']
+
+
+    return render(request,'simple_app/index.html',context); 
--- simple_project/simple_project/wsgi.py
+++ simple_project/simple_project/wsgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 import sys
+import io
 
 from django.core.wsgi import get_wsgi_application
 
@@ -16,12 +17,33 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simple_project.settings")
 
 application = get_wsgi_application()
 
-def call_application(environ):
-    environ['wsgi.input'] = sys.stdin
+def call_application(environ,postdata='',header=''):
+    inputStream=io.BytesIO()
+    
+    environ['wsgi.input'] = inputStream
     environ['wsgi.errros'] = sys.stderr
     environ['wsgi.version']= (1,0)
+    print(header)
+    if postdata:
+        print(postdata)
+        print(sys.version)
+        environ['wsgi.input']=io.BytesIO(postdata.encode('utf-8'))
+        environ['CONTENT_LENGTH']=str(len(postdata.encode('utf-8')))
+        environ['CONTENT_TYPE']='application/x-wwww-form-urlencoded'
+    if 'Cookie' in header:
+        environ['HTTP_COOKIE']=header['Cookie']
+    if 'User-Agent' in header:
+        environ['HTTP_USER_AGENT']=header['User-Agent']
+    if 'Host' in header:
+        environ['HTTP_HOST']=header['Host']
+    if 'Content-Type' in header:
+        environ['CONTENT_TYPE']=header['Content-Type']
+    if 'Accept' in header:
+        environ['HTTP_ACCEPT'] = header['Accept']
+    if 'Accept-Encoding' in header:
+        environ['HTTP_ACCEPT_ENCODING'] =header['Accept-Encoding']
+
     headers_set = []
-    
     def write(data):
         #sys.stdout.write(data)
         sys.stdout.flush()
@@ -29,7 +51,6 @@ def call_application(environ):
     def start_response(status,response_headers,exc_info=None):
         headers_set[:]=[status,response_headers]
         return write
-
     result=application(environ,start_response)
     stringresult=''.join([x.decode('utf-8') for x in result])
     try:
